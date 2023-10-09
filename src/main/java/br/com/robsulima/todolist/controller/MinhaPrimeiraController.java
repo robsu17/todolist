d2664e21f4c3e03efcc0e57b3ee2566910407c35
Robson Wendel <robson.costa134@gmail.com>
2023-10-09 11:05:12 -0300

    Create Project

---
+++ src/main/java/br/com/robsulima/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,11 @@
+package br.com.robsulima.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class MinhaPrimeiraController
+{
+
+}
