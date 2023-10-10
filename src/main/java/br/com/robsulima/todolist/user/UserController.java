58a0966e58faf53193bdb76fe36f673f2948cb55
Robson Wendel <robson.costa134@gmail.com>
2023-10-10 15:16:11 -0300

    Create model user

--- src/main/java/br/com/robsulima/todolist/user/UserController.java
+++ src/main/java/br/com/robsulima/todolist/user/UserController.java
@@ -1,24 +1,36 @@
 package br.com.robsulima.todolist.user;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Modificadores
+ * Modificadores:
  * Public
  * Private
  * Protected
  */
+
 @RestController
 @RequestMapping("/users")
 public class UserController
 {
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/register")
-    public void create(@RequestBody UserModel userModel)
+    public ResponseEntity create(@RequestBody UserModel user)
     {
-        System.out.println(userModel.name);
+        var username = this.userRepository.findByUsername(user.getUsername());
+        if (username != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        var userCreated = this.userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
